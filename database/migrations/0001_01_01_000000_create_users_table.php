cbfa70cdedf9cb5834cebf4364843619924eee14
Jayvee <jayvee.github0703@gmail.com>
2026-05-13 16:02:57 +0800

    Setup and Install Passport and Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("middle_initital")->nullable();
+            $table->date("birthdate");
+
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreignId("role_id");
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
